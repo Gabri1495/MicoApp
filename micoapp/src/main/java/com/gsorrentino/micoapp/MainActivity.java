187518a6421527a6cd3b8850ffc04ab590ebce51
Gabri1495 <gabry1495@gmail.com>
2019-04-21 17:22:08 +0200

    Prima versione prototipale dell'app. ° Creata la basilare grafica dei menu ° Aggiunti listeners per l'assistenza

---
+++ micoapp/src/main/java/com/gsorrentino/micoapp/MainActivity.java
@@ -0,0 +1,126 @@
+package com.gsorrentino.micoapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            // Apre una chat su Telegram per contattare lo sviluppatore
+            case R.id.action_telegram:
+                Intent telegramIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/Gabri1495"));
+                if(telegramIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(telegramIntent);
+                return true;
+
+            // Prepara una mail in parte precompilata per contattare lo sviluppatore
+            case R.id.action_email:
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"gabry1495@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject) + getString(R.string.app_name));
+                emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text));
+
+                if(emailIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(emailIntent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_home:
+                //TODO
+            case R.id.menu_map:
+                //TODO
+            case R.id.menu_encyclopedia:
+                //TODO
+            case R.id.menu_archive:
+                //TODO
+            case R.id.menu_memories:
+                //TODO
+            case R.id.menu_statistics:
+                //TODO
+            case R.id.menu_settings:
+                //TODO
+            case R.id.menu_send:
+                //TODO
+            case R.id.menu_history:
+                //TODO
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
